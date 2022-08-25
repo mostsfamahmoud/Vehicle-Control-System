f86c225c4169e44dceeeff3394e219315d98081d
Mostafa Mahmoud <72917420+mostsfamahmoud@users.noreply.github.com>
2022-08-26 00:41:15 +0200

    Implementation

---
+++ Vehicle Control System/src/main.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Mostafa Mahmoud Ali
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Main Function of Vehicle Control System Project
+ ============================================================================
+ */
+
+#include "Vehicle.h"
+
+int main(void) {
+
+	Vehicle v1;
+
+	vehicle_Init(&v1);
+
+	printf("============= PS: Upper & Lower-case characters are accepted =============\n\n");
+
+	startVehicleMenu(&v1);
+
+	return EXIT_SUCCESS;
+}
